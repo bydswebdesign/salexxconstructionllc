674cca9f9a092a66261dae7f7202785a676a53eb
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-07-19 10:56:01 -0700

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
 
 function App() {
-
   return (
     <div>
-      <h1 className='text-8xl font-bold text-center'>Salexx Construction LLC</h1>
+      <div className="flex justify-center items-center">
+        <h1 className="text-8xl font-bold">Salexx Construction LLC</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
