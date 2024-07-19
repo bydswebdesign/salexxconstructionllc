1c4098bf9831b1ef2f39728a03b76c3dcd11c082
Batmanninja1991 <109576315+Batmanninja1991@users.noreply.github.com>
2024-07-19 11:21:05 -0700

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./index.css";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
-      <div className="flex justify-center items-center">
-        <h1 className="text-8xl font-bold">Salexx Construction LLC</h1>
-      </div>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="w-full h-screen flex justify-between items-center">
+        <h1>Salexx Concrete LLC</h1>
+        <p>Concrete done with a passion</p>
+        <div className="w-full h-[200px] bg-blue-300 rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
